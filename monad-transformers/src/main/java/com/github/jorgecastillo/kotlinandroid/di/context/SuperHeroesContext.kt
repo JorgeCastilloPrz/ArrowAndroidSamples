194499050bd43aa042bc18c55d82abf62b218c4a
Raul Raja <raulraja@gmail.com>
2017-09-30 00:27:28 +0200

    Adapted Monad Transformers example to Kategory 0.3.9

--- monad-transformers/src/main/java/com/github/jorgecastillo/kotlinandroid/di/context/SuperHeroesContext.kt
+++ monad-transformers/src/main/java/com/github/jorgecastillo/kotlinandroid/di/context/SuperHeroesContext.kt
@@ -2,6 +2,7 @@ package com.github.jorgecastillo.kotlinandroid.di.context
 
 import android.content.Context
 import com.github.jorgecastillo.kotlinandroid.BuildConfig
+import com.github.jorgecastillo.kotlinandroid.presentation.HeroesView
 import com.github.jorgecastillo.kotlinandroid.presentation.SuperHeroDetailView
 import com.github.jorgecastillo.kotlinandroid.presentation.SuperHeroesListView
 import com.karumi.marvelapiclient.CharacterApiClient
@@ -9,12 +10,14 @@ import com.karumi.marvelapiclient.MarvelApiConfig.Builder
 
 sealed class SuperHeroesContext(ctx: Context) {
 
+  abstract val view: HeroesView
+
   val heroDetailsPage = com.github.jorgecastillo.kotlinandroid.presentation.navigation.HeroDetailsPage()
   val apiClient
     get() = CharacterApiClient(Builder(
         BuildConfig.MARVEL_PUBLIC_KEY,
         BuildConfig.MARVEL_PRIVATE_KEY).debug().build())
 
-  data class GetHeroesContext(val ctx: Context, val view: SuperHeroesListView) : SuperHeroesContext(ctx)
-  data class GetHeroDetailsContext(val ctx: Context, val view: SuperHeroDetailView) : SuperHeroesContext(ctx)
+  data class GetHeroesContext(val ctx: Context, override val view: SuperHeroesListView) : SuperHeroesContext(ctx)
+  data class GetHeroDetailsContext(val ctx: Context, override val view: SuperHeroDetailView) : SuperHeroesContext(ctx)
 }
