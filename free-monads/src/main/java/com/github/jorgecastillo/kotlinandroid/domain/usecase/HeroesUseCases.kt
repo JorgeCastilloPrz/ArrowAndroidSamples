2fed37bb71b35136bc3ba1b6214e35dec6a04690
Jorge Castillo <jorge.castillo.prz@gmail.com>
2017-10-06 19:16:20 +0200

    fix use cases

--- free-monads/src/main/java/com/github/jorgecastillo/kotlinandroid/domain/usecase/HeroesUseCases.kt
+++ free-monads/src/main/java/com/github/jorgecastillo/kotlinandroid/domain/usecase/HeroesUseCases.kt
@@ -1,22 +1,17 @@
 package com.github.jorgecastillo.kotlinandroid.domain.usecase
 
 import com.github.jorgecastillo.kotlinandroid.data.CachePolicy.NetworkOnly
+import com.github.jorgecastillo.kotlinandroid.data.algebra.FreeHeroesAlgebra
 import com.github.jorgecastillo.kotlinandroid.data.getHeroDetails
 import com.github.jorgecastillo.kotlinandroid.data.getHeroesFromAvengerComicsWithCachePolicy
 import com.github.jorgecastillo.kotlinandroid.data.getHeroesWithCachePolicy
-import com.github.jorgecastillo.kotlinandroid.di.context.SuperHeroesContext.GetHeroDetailsContext
-import com.github.jorgecastillo.kotlinandroid.di.context.SuperHeroesContext.GetHeroesContext
-import com.github.jorgecastillo.kotlinandroid.domain.model.CharacterError
-import com.github.jorgecastillo.kotlinandroid.functional.MonadControl
-import com.github.jorgecastillo.kotlinandroid.functional.monadControl
 import com.karumi.marvelapiclient.model.CharacterDto
-import kategory.HK
 
-inline fun <reified F> getHeroesUseCase(): HK<F, List<CharacterDto>> =
+fun getHeroesUseCase(): FreeHeroesAlgebra<List<CharacterDto>> =
     getHeroesWithCachePolicy(NetworkOnly)
 
-inline fun <reified F> getHeroDetailsUseCase(heroId: String): HK<F, CharacterDto> =
+fun getHeroDetailsUseCase(heroId: String): FreeHeroesAlgebra<List<CharacterDto>> =
     getHeroDetails(NetworkOnly, heroId)
 
-inline fun <reified F> getHeroesFromAvengerComicsUseCase(): HK<F, List<CharacterDto>> =
+fun getHeroesFromAvengerComicsUseCase(): FreeHeroesAlgebra<List<CharacterDto>> =
     getHeroesFromAvengerComicsWithCachePolicy(NetworkOnly)
