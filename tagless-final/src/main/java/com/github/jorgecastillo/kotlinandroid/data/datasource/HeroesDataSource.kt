5af3c98233d613219c4121ccb462a475cb44efab
Jorge Castillo <jorge.castillo.prz@gmail.com>
2017-10-06 12:58:38 +0200

    remove some unused classes

--- tagless-final/src/main/java/com/github/jorgecastillo/kotlinandroid/data/datasource/HeroesDataSource.kt
+++
@@ -1,8 +0,0 @@
-package com.github.jorgecastillo.kotlinandroid.data.datasource
-
-import com.github.jorgecastillo.kotlinandroid.domain.model.SuperHero
-
-interface HeroesDataSource {
-
-  fun getAll(): List<SuperHero>
-}
--- tagless-final/src/main/java/com/github/jorgecastillo/kotlinandroid/data/datasource/remote/DataMappers.kt
+++
@@ -1,6 +0,0 @@
-package com.github.jorgecastillo.kotlinandroid.data.datasource.remote
-
-import com.github.jorgecastillo.kotlinandroid.domain.model.SuperHero
-import com.karumi.marvelapiclient.model.CharacterDto
-
-fun mapApiCharacterToSuperHero(character: CharacterDto) = SuperHero(character.name)
